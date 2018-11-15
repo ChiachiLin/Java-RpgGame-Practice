445833cbdb8c4c49e7f9528010a010baa0ef8e80
JackyLin <jacky21807@gmail.com>
2018-11-15 21:05:23 +0800

    modify Demo.java

--- src/Demo.java
+++ src/Demo.java
@@ -7,6 +7,7 @@ public class Demo {
 	private static Scanner scanner;
 	private static int job;
 	private static String name;
+	private static String chioce;
 
 	public static void main(String args[]) {
 		System.out.println("Welcome to Newworld! ");
@@ -17,32 +18,36 @@ public class Demo {
 		System.out.println("The folloeing are jobs' information. ");
 		System.out.println("[1]SwordsMan HP:100 Atk:10");
 		System.out.println("[2]Magician  HP:80  Atk:15");
-		job();
+		chiocejob();
+			
 	}
 	public static void named() {
 		scanner = new Scanner(System.in);
 		name = scanner.nextLine();
 		System.out.println("Hello!" + name);
 	}
-	public static void job() {
+	public static String chiocejob() {
 		try{scanner = new Scanner(System.in);
-		System.out.print("Your choice is ? :");
+		System.out.print("Your chioce is ? :");
 		job = scanner.nextInt();
 		if (job == 1) {
 			new SwordsMan(name, 100, 1, 10);
+			
 			System.out.println("Congratulations! You become a Swordsman. ");
-		} else if (job == 2) {
+				} else if (job == 2) {
 			new Magician(name, 80, 1, 15);
 			System.out.println("Congratulations! You become a Magician. ");
 		} else {
 			System.out.println("The chioce is not in the list! please choose again!");
-			job();
+			chiocejob();
 		}}
 		catch(InputMismatchException e) {
 			System.out.printf("Wrong input¡G%s%n", scanner.next());
 			System.out.println("Please choose again!");
-			job();
+			chiocejob();
 			}
-		}
+		return chioce;
+				}
+					
 	}
 
