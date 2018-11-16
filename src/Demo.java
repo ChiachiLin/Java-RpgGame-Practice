b50dcd373a48afb48af23f28f11a0513f583b372
JackyLin <jacky21807@gmail.com>
2018-11-16 13:37:26 +0800

    modify Demo.java

--- src/Demo.java
+++ src/Demo.java
@@ -7,6 +7,7 @@ public class Demo {
 	private static Scanner scanner;
 	private static int job = 0;
 	private static String name;
+	private static int chioce;
 
 	public static void main(String args[]) {
 		// Named
@@ -19,7 +20,7 @@ public class Demo {
 		System.out.println("[1]SwordsMan HP:100 Atk:10");
 		System.out.println("[2]Magician  HP:80  Atk:15");
 		chiocejob();
-
+		System.out.print(chioce);
 	}
 
 	public static void named() {
@@ -28,16 +29,19 @@ public class Demo {
 		System.out.println("Hello!" + name);
 	}
 
-	public static SwordsMan chiocejob() {
+	public static int chiocejob() {
 		try {
 			scanner = new Scanner(System.in);
 			System.out.print("Your chioce is ? :");
 			job = scanner.nextInt();
 			if (job == 1) {
-				SwordsMan chioce = new SwordsMan(name, 100, 1, 10);
+				//SwordsMan chioce = new SwordsMan(name, 100, 1, 10);
+				System.out.println("Congratulations! You become a SwordsMan. ");
+				chioce = 1 ;
 			} else if (job == 2) {
-				Magician chioce = new Magician(name, 80, 1, 15);
+				//Magician chioce = new Magician(name, 80, 1, 15);
 				System.out.println("Congratulations! You become a Magician. ");
+				chioce = 2 ;
 			} else {
 				System.out.println("The chioce is not in the list! please choose again!");
 				chiocejob();
@@ -48,8 +52,9 @@ public class Demo {
 			System.out.println("Please choose again!");
 			chiocejob();
 		}
-		return null;
+		return chioce;
 		
+
 	}
 
 }
